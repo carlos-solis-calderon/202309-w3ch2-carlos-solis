5ff869c070ace64a9d509bb2cab5210f8a08e9e6
Carlos Solis <mrsoliscalderon@gmail.com>
2023-10-24 18:56:17 +0200

    Add css/sass files

--- src/characters.ts
+++ src/characters.ts
@@ -4,9 +4,9 @@ import { Squire } from './esquire';
 import { Advisor } from './advisor';
 
 const Joffrey = new King('Joffrey', 'Baratheon', 16, 2);
-const Daenerys = new Fighter('Daenerys', 'Targaryen', 30, 'Dragones', 10);
+const Daenerys = new Fighter('Daenerys', 'Targaryen', 30, 'Dragones', 10)
 const Bronn = new Squire('Bronn', 'AguasNegras', 54, 0);
-const Tyrion = new Advisor('Tyrion', 'Lannister', 40, 'daenerys');
-const Jaime = new Fighter('Jaime', 'Lannister');
+const Tyrion = new Advisor('Tyrion', 'Lannister', 40, 'Daenerys')
+const Jaime = new Fighter('Jaime', 'Lannister', 30, "Espada");
 
 export const characters = [Joffrey, Daenerys, Bronn, Tyrion, Jaime];
